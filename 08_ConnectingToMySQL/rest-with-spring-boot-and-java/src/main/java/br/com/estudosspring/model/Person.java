4e9cfe691a5ec04bc4a7babe08b807a7ade768e3
Rafael <rafael_gabriel623@hotmail.com>
2023-01-30 21:39:21 -0300

    Connecting to MySQL with Spring Data, JPA and Hibernate

---
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java/src/main/java/br/com/estudosspring/model/Person.java
@@ -0,0 +1,87 @@
+package br.com.estudosspring.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
+    private String lasttName;
+
+    @Column(name = "adress", nullable = false, length = 100)
+    private String address;
+
+    @Column(name = "gender", nullable = false, length = 6)
+    private String gender;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasttName() {
+        return lasttName;
+    }
+
+    public void setLasttName(String lasttName) {
+        this.lasttName = lasttName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id.equals(person.id) && firstName.equals(person.firstName) && lasttName.equals(person.lasttName) && address.equals(person.address) && gender.equals(person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lasttName, address, gender);
+    }
+}
---
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java/src/main/java/br/com/estudosspring/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package br.com.estudosspring.repositories;
+
+import br.com.estudosspring.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
