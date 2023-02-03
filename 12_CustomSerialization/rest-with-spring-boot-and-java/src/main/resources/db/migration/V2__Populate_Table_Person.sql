2fe868ded6609dd8d8e3d18ebf9a68666855a9eb
Rafael <rafael_gabriel623@hotmail.com>
2023-02-02 22:15:03 -0300

    Custom JSON serialization

---
+++ 12_CustomSerialization/rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` VALUES
+                        (1,'Rafael','Gabriel','Maceió','Male'),
+                        (2,'Ayrton','Senna','São Paulo','Male'),
+                        (3,'Leonardo','da Vinci','Anchiano - Italy','Male'),
+                        (5,'Indira','Gandhi','Porbandar - India','Female'),
+                        (6,'Mahatma','Gandhi','Porbandar - India','Male'),
+                        (9,'Muhammad','Ali','Kentucky - US','Male'),
+                        (10,'Nelson','Mandela','Mvezo - South Africa','Male'),
+                        (11,'Nikola','Tesla','Smiljan - Croatia','Male');
